0ccf443c79a0a2f58f450a19255c6c28c26426ed
balagani balaji <balagajigoud79@gmail.com>
2026-01-31 17:51:07 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -61,7 +61,7 @@ VALIDATE $? "Uzip catalogue code"
 npm install  &>>$LOGS_FILE
 VALIDATE $? "Installing dependencies"
 
-cp $SCRIPT_DIR /catalogue.service /etc/systemd/system/catalogue.service
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "Created systemctl service"
 
 systemctl daemon-reload
