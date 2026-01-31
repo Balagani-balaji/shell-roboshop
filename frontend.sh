2269d4e0724c7433d3d3f56a939a3bcc0fd3987c
balagani balaji <balagajigoud79@gmail.com>
2026-01-31 21:40:42 +0530

    actions

---
+++ frontend.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf disable ngnix -y &>>$LOGS_FILE
+VALIDATE $? "disable ngnix server"
+
+dnf enable ngnix:1.24 -y &>>$LOGS_FILE
+VALIDATE $? "Enable ngnix"
+
+dnf install ngnix -y &>>$LOGS_FILE
+VALIDATE $? "Installing ngnix server"
+
+systemctl enable ngnix &>>$LOGS_FILE
+VALIDATE $? "Enable ngnix"
+
+systemctl start ngnix &>>$LOGS_FILE
+VALIDATE $? "Start ngnix"
+
+
